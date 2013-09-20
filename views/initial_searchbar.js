2e4bbc21c22533793dc487263a484ba0cd918972
Ilya Kavalerov <ilyakavalerov@gmail.com>
2013-09-20 15:19:23 -0400

    Change url on search

--- views/initial_searchbar.js
+++ views/initial_searchbar.js
@@ -2,9 +2,19 @@ ES.Views.InitialSearchbar = Backbone.View.extend({
   id: 'search-container',
   className: 'group',
 
+  events: {
+    "submit form": "search"
+  },
+
   render: function () {
     var searchBar = new EJS({url: 'templates/initial_searchbar'}).render();
     this.$el.html(searchBar);
     return this;
+  },
+
+  search: function (event) {
+    event.preventDefault();
+    var searchTerm = $(event.target).children(":first").val();
+    Backbone.history.navigate("#/" + encodeURIComponent(searchTerm));
   }
 });
