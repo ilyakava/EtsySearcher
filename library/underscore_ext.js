c39793918473567ce42c37a7f230dd0e6fd632f6
Ilya Kavalerov <ilyakavalerov@gmail.com>
2013-09-22 19:02:10 -0400

    Add convenience methods for going between form, URI, and obj

--- library/underscore_ext.js
+++ library/underscore_ext.js
@@ -4,3 +4,17 @@ _.mixin({
     return string.charAt(0).toUpperCase() + string.substring(1).toLowerCase();
   }
 });
+
+_.mixin({
+  uriifyForm : function(jQueryForm) {
+    var obj = $.extend.apply(null, jQueryForm.serializeArray());
+    return JSON.stringify(obj);
+  }
+});
+
+_.mixin({
+  objectifyURI : function(uriStr) {
+    var searchParams = decodeURIComponent(uriStr);
+    return JSON.parse(decodeURIComponent(searchParams));
+  }
+});
