569ea3428efa6ec00b1f53f29622f1827cd1eb3c
Ilya Kavalerov <ilyakavalerov@gmail.com>
2013-09-21 21:18:44 -0400

    Sematic changes, listing/listings (from API) --> listing/results (when on the page)

--- router.js
+++ router.js
@@ -2,7 +2,8 @@ ES.Routers.Router = Backbone.Router.extend({
   initialize: function ($rootEl) {
     this.$rootEl = $rootEl;
     this.$searchEl = $rootEl.find('#search-container');
-    this.listings = new ES.Collections.Listings();
+    this.$resultsEl = $rootEl.find('#search-container');
+    this.results = null;
   },
 
   routes: {
@@ -12,18 +13,25 @@ ES.Routers.Router = Backbone.Router.extend({
   },
 
   home: function () {
-    var that = this;
-
     var searchBar = new ES.Views.InitialSearchbar();
     this.$searchEl.html(searchBar.render().$el);
   },
 
   firstSearch: function (urlSearch) {
+    var that = this;
+
     var searchTerm = decodeURIComponent(urlSearch);
+    this.results = new ES.Collections.Listings(searchTerm).populate();
 
     // rendering a new searchbar because now form submission will
     // do a different action, refinning search instead of API call search
     var searchBar = new ES.Views.RefineSearchbar();
     this.$searchEl.html(searchBar.render().$el);
+
+    // insert listings on the page for the first time
+    var resultsView = new ES.Views.ResultsList({
+      collection: that.results
+    });
+
   }
 });
