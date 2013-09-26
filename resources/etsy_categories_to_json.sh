b4c2af67701688b7bcf3d3173fb4bf56f43a8359
Ilya Kavalerov <ilyakavalerov@gmail.com>
2013-09-26 18:20:00 -0400

    Add a shell script (and its output) to parse the webpage that Etsy lists its categories and subcategories (which otherwise are innaccessible...as far as I have found)

---
+++ resources/etsy_categories_to_json.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# will output key/value pairs of Etsy's categories/sub-categories
+# these are to be used in the construction of the 'category' parameter
+# in Etsy API calls, About:
+# http://www.etsy.com/developers/documentation/getting_started/api_basics#type_category
+
+# NOTE that this script relies on accessing the url that Etsy displays all of its
+# categories at visually (html), and parsing the urls that are href-ed
+
+touch etsy_cat.json
+echo "[" > etsy_cat.json
+
+curl http://www.etsy.com/categories?ref=fp_nav_colors | \
+gawk 'match($0, /<a href="\/listing-category\/(.*)\/(.*)"/, ary) {print ary[1],ary[2]}' | \
+sed 's/\([a-z,_]*\) \([a-z,_,0-9]*\)/\    {"\1": "\2"},/' | \
+sed '$ s/\(.*\),/\1/' >> etsy_cat.json
+
+echo "]" >> etsy_cat.json
